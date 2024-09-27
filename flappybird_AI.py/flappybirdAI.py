937f2ed30d4b1fcf4790a1c61e7c339b88cc5c65
prabal-17 <prabalpratap018@gmail.com>
2024-09-28 00:49:33 +0530

    Implement Base movement and Pipe class

--- flappybird_AI.py/flappybirdAI.py
+++ flappybird_AI.py/flappybirdAI.py
@@ -1,42 +1,31 @@
-
 import pygame
 import random
 import os
-import neat
 
-WIN_WIDTH = 600
-WIN_HEIGHT = 800
-FLOOR = 730
-STAT_FONT = pygame.font.SysFont("comicsans", 50)
-END_FONT = pygame.font.SysFont("comicsans", 70)
-DRAW_LINES = False
+WIN_WIDTH = 400
+WIN_HEIGHT = 600
 
-WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+# Initialize Pygame and the display
+pygame.init()
+win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
-pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
-bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","bg.png")).convert_alpha(), (600, 900))
-bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird" + str(x) + ".png"))) for x in range(1,4)]
-base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")).convert_alpha())
+# Load images after display is initialized
+pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")).convert_alpha())
+bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs", "bg.png")).convert_alpha(), (600, 900))
+bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird" + str(x) + ".png"))) for x in range(1, 4)]
+base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")).convert_alpha())
 
 gen = 0
 
 class Bird:
-    """
-    Bird class representing the flappy bird
-    """
+    """ Bird class """
     MAX_ROTATION = 25
     IMGS = bird_images
     ROT_VEL = 20
-    ANIMATION_TIME = 5
+    ANIMATION_TIME = 5  # Animation time
 
     def __init__(self, x, y):
-        """
-        Initialize the object
-        :param x: starting x pos (int)
-        :param y: starting y pos (int)
-        :return: None
-        """
         self.x = x
         self.y = y
         self.tilt = 0  # degrees to tilt
@@ -47,68 +36,115 @@ class Bird:
         self.img = self.IMGS[0]
 
     def jump(self):
-        """
-        make the bird jump
-        :return: None
-        """
         self.vel = -10.5
         self.tick_count = 0
         self.height = self.y
 
     def move(self):
-        """
-        make the bird move
-        :return: None
-        """
         self.tick_count += 1
+        displacement = self.vel * self.tick_count + 0.5 * 3 * self.tick_count ** 2  # s=ut + 1/2 at^2
 
-        # for downward acceleration
-        displacement = self.vel*(self.tick_count) + 0.5*(3)*(self.tick_count)**2  # calculate displacement
-
-        # terminal velocity
+        # Terminal velocity
         if displacement >= 16:
-            displacement = (displacement/abs(displacement)) * 16
+            displacement = (displacement / abs(displacement)) * 16
 
         if displacement < 0:
             displacement -= 2
 
-        self.y = self.y + displacement
+        self.y += displacement
 
-        if displacement < 0 or self.y < self.height + 50:  # tilt up
+        if displacement < 0 or self.y < self.height + 50:
             if self.tilt < self.MAX_ROTATION:
                 self.tilt = self.MAX_ROTATION
-        else:  # tilt down
+        else:
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL
 
     def draw(self, win):
-        """
-        draw the bird
-        :param win: pygame window or surface
-        :return: None
-        """
         self.img_count += 1
 
         # For animation of bird, loop through three images
         if self.img_count <= self.ANIMATION_TIME:
             self.img = self.IMGS[0]
-        elif self.img_count <= self.ANIMATION_TIME*2:
+        elif self.img_count <= self.ANIMATION_TIME * 2:
             self.img = self.IMGS[1]
-        elif self.img_count <= self.ANIMATION_TIME*3:
+        elif self.img_count <= self.ANIMATION_TIME * 3:
             self.img = self.IMGS[2]
-        elif self.img_count <= self.ANIMATION_TIME*4:
+        elif self.img_count <= self.ANIMATION_TIME * 4:
             self.img = self.IMGS[1]
-        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
             self.img = self.IMGS[0]
             self.img_count = 0
 
-        # so when bird is nose diving it isn't flapping
+        # So when bird is nose diving, it isn't flapping
         if self.tilt <= -80:
             self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME*2
+            self.img_count = self.ANIMATION_TIME * 2
 
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
+        win.blit(rotated_image, new_rect.topleft)
 
-        # tilt the bird
-        # blitRotateCenter(win, self.img, (self.x, self.y), self.tilt)
-
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
     
+
+class Base:
+    """
+    Represents the moving floor of the game
+    """
+    VEL = 5
+    WIDTH = base_img.get_width()
+    IMG = base_img
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        """
+        Move floor so it looks like it is scrolling
+        """
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        """
+        Draw the floor. This is two images that move together.
+        """
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, bird, base):
+    win.blit(bg_img, (0, 0))
+    bird.draw(win)
+    base.draw(win)  # Draw the moving base
+    pygame.display.update()
+
+def main():
+    bird = Bird(200, 200)
+    base = Base(500)  # Position the base at the bottom
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        bird.move()
+        base.move()  # Move the base
+        draw_window(win, bird, base)  # Pass the base to draw_window()
+
+    pygame.quit()
+
+main()
